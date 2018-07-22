89569d9be1f880c231b42b9e15676c4a0ff807cb
Yutthapichai <cancel_go@hotmail.com>
2018-07-22 22:20:18 +0700

    Create front-end register and login

--- back_end/routes/api/users.js
+++ back_end/routes/api/users.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const router = express();
-const User = require('../../models/User');
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
+const express = require('express')
+const router = express()
+const User = require('../../models/User')
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 
 router.get('/test', (req, res) => {
-    res.json({ msg: 'users work'});
-});
+    res.json({ msg: 'users work'})
+})
 
 router.post('/register', (req, res) => {
     User.findOne({ email: req.body.email }).then(
         user => {
             if(user) {
-                res.status(400).json({ email: 'Email already exists'});
+                res.status(400).json({ email: 'Email already exists'})
             }else {
-                const avatar = gravatar.url(req.body.email, { s: '200',r: 'pg',d: 'mm'});
+                const avatar = gravatar.url(req.body.email, { s: '200',r: 'pg',d: 'mm'})
                 const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
                     avatar,
                     password: req.body.password
-                });
+                })
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
-                        if (err) throw err;
-                        newUser.password = hash;
+                        if (err) throw err
+                        newUser.password = hash
                         newUser.save()
                         .then( user => res.json(user))
-                        .catch( err => console.log(err));
-                    });
-                });
+                        .catch( err => console.log(err))
+                    })
+                })
             }
         }
-    );
-});
+    ).catch( err => res.status(500).json({ msg: 'errr' + err}))
+})
 
-module.exports = router;
+module.exports = router
